6e2ce578726d85d5444360830dafa3f908f1f27c
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-03-20 00:46:57 +0100

    Konfiguracija projekta

---
+++ ProjekatISA/src/main/java/projekatISA/ProjekatIsaApplication.java
@@ -0,0 +1,12 @@
+package projekatISA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatIsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatIsaApplication.class, args);
+	}
+}
