8e6c3eaeaf571c24ce71d6a99e98bd35f8f61645
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-03-26 21:20:46 +0200

    Controller

---
+++ ProjekatISA/src/main/java/projekatISA/controller/TheatreController.java
@@ -0,0 +1,33 @@
+package projekatISA.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import projekatISA.domein.Theatre;
+import projekatISA.service.TheatreService;
+
+@RestController
+@RequestMapping(value="/api/theatres")
+public class TheatreController {
+
+	@Autowired
+	private TheatreService theatreService; 
+	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Theatre> getTheatre(@PathVariable Long id){
+		System.out.println("aaaaa");
+		Theatre theatre = theatreService.findOne(id);	
+		if(theatre == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(theatre, HttpStatus.OK);
+	}
+	
+	
+}
