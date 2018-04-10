0218667e7db4863fd2b6b1547ccf19dffff298f5
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-04-11 01:10:22 +0200

    import

--- ProjekatISA/src/main/resources/import.sql
+++ ProjekatISA/src/main/resources/import.sql
@@ -7,8 +7,8 @@ INSERT INTO `isa`.`cinematheatre` (`id`,`name`,`description`,`address`,`rating`,
 INSERT INTO `isa`.`cinematheatre` (`id`,`name`,`description`,`address`,`rating`,`cinema`,`repertory_id`) VALUES ('4' ,'Umetnicko pozoriste','srednje','Bulevar Oslobodjenja 100','3',0,'2');
 INSERT INTO `isa`.`cinematheatre` (`id`,`name`,`description`,`address`,`rating`,`cinema`,`repertory_id`) VALUES ( '5','Bioskop','vrlo dobar','Mihajla Pupina 11','4',1,'4');
 
+INSERT INTO `isa`.`user` (`id`, `email`, `name`, `surname`, `password`, `city`, `phone_number`, `active`) VALUES ('2', 'dragan.dulic1@gmail.com', 'Dragan', 'Dulic', 'IsaIsic1.', 'Novi Sad', '061555333', 1);
 
-INSERT INTO `isa`.`user` (`id`, `email`, `name`, `surname`, `password`, `city`, `phone_number`, `active`) VALUES ('2', 'dragan.dulic1@gmail.com', 'Dragan', 'Dulic', 'IsaIsic1.', 'Novi Sad', '061555333', 1);  
 
 
 INSERT INTO `isa`.`thematicprops` (`id`, `description`, `name`, `price`, `cinema_theatre_id`,`user_id`,`reserved`) VALUES ('1','suncane naocar','naocareee','500','1','1',0);
